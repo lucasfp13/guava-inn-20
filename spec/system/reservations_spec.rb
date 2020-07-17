e3bbe20a833855ad2f7f4e0c501d03ee0138b7dd
Lucas Portela <lucasfjportela@gmail.com>
2020-07-17 02:14:04 -0300

    test: Started implementation of Reservation's integration tests

--- spec/system/reservations_spec.rb
+++ spec/system/reservations_spec.rb
@@ -1,9 +1,40 @@
-RSpec.describe 'Rooms', type: :system do
+require 'rails_helper'
+
+RSpec.describe 'Reservations', type: :system do
   before do
     driven_by(:selenium_chrome_headless)
   end
 
   describe 'searching' do
-    it 'allows users to search for available rooms with a given capacity in a period'
+    before do
+      Room.create!(code: '101', capacity: 2)
+    end
+
+    it 'allows users to search for available rooms with a given capacity in a period' do
+      visit search_reservations_path
+
+      expect(page).to have_content('New Reservation')
+
+      fill_in 'From', with: '08/20/2020'
+      fill_in 'To', with: '08/23/2020'
+      select '2', from: '# of guests'
+      click_on 'Search for Available Rooms'
+
+      expect(page).to have_content('Available Rooms')
+
+      within('table') do
+        within('thead') do
+          expect(page).to have_content('Code')
+          expect(page).to have_content('Capacity')
+          expect(page).to have_content('Actions')
+        end
+
+        within('tbody tr:first-child') do
+          expect(page).to have_content('101')
+          expect(page).to have_content('2 people')
+          expect(page).to have_link('Create Reservation')
+        end
+      end
+    end
   end
 end
