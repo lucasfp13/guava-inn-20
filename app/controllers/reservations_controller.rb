b3265804efd34b1514fb256dadfc6d107aa22271
Lucas Portela <lucasfjportela@gmail.com>
2020-07-17 03:26:49 -0300

    style: Set condition to show search results in a before_action method

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,8 +1,7 @@
 class ReservationsController < ApplicationController
+  before_action :set_should_show_results, only: %i[search]
+
   def search
-    @should_show_results = params[:start_date].present? &&
-      params[:end_date].present? &&
-      params[:number_of_guests].present?
     @available_rooms = @should_show_results ? Room.all : Room.none
   end
 
@@ -28,6 +27,12 @@ class ReservationsController < ApplicationController
   end
 
   private
+
+  def set_should_show_results
+    @should_show_results = params[:start_date].present? &&
+      params[:end_date].present? &&
+      params[:number_of_guests].present?
+  end
 
   def reservation_params
     params.require(:reservation).permit(:start_date, :end_date, :number_of_guests, :guest_name, :room_id)
