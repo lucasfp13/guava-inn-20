ab251b849e6ef2a5a12428d0ae4ab47445674b2b
Lucas Portela <lucasfjportela@gmail.com>
2020-07-21 04:09:30 -0300

    fix: Fixes in calculation of weekly and monthly occupancy rate.

--- app/models/room.rb
+++ app/models/room.rb
@@ -18,25 +18,32 @@ class Room < ApplicationRecord
 
   private
 
-  def occupancy_rate_calculation(period)
+  def occupancy_rate_calculation(next_days)
     return 0 if reservations.blank?
 
     ###
-    # using DateTime to work with local time zone system, but needs to convert
-    # back to the Date format to match with Reservation model 'date' field type
-    tomorrow = (DateTime.now + 1).to_date
-    period_range = (tomorrow..(tomorrow + period)).to_a
+    # Using DateTime to work with local time zone system, but needs to convert
+    # back to the Date format to match with Reservation model 'date' field type.
+    tomorrow = (DateTime.now + 1.day).to_date
+    selected_period = (tomorrow..7.days.from_now).to_a
 
     days_with_reservation = 0
     reservations.each do |reservation|
-      reservation_range = (reservation.start_date..reservation.end_date).to_a
       ###
-      # using the bitwise AND operator to get the intersection between
-      # period range and each reservation date range and counting them
-      days_with_reservation += (period_range & reservation_range).count
+      # Skipping rooms from past periods than selected period
+      next if reservation.end_date < tomorrow
+      ###
+      # Note that on each reservation case the end_date shouldn't be considered
+      # as a 'occupied day' since the end_date ends at 12:00 PM and the room is
+      # free after this. So, we don't count with the last day of reservation.
+      reservation_period = (reservation.start_date..(reservation.end_date - 1.day)).to_a
+      ###
+      # Using the bitwise AND operator to get the intersection between period
+      # range and each reservation date range and incrementing them.
+      days_with_reservation += (selected_period & reservation_period).count
     end
 
-    ((days_with_reservation.to_f / period) * 100).round
+    ((days_with_reservation.to_f / next_days) * 100).round
   end
 
   def calculate_occupancy_rates
