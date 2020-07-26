a0d68aebe699bc25bf9c42d3676f278db8872636
Lucas Portela <lucasfjportela@gmail.com>
2020-07-25 22:14:10 -0300

    feat: Add transient fields to relate both of model factories

--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -1,9 +1,19 @@
 FactoryBot.define do
   factory :reservation do
     room
-    start_date { rand(0..50).days.ago.to_date }
-    start_date { rand(1..50).days.from_now.to_date }
+    start_date { rand(0..20).days.ago.to_date }
+    end_date { rand(1..50).days.from_now.to_date }
     guest_name { Faker::Name.unique.name }
     number_of_guests { room.present? ? rand(1..room.capacity) : rand(1..10) }
+
+    transient do
+      in_room { {} }
+    end
+
+    after(:build) do |reservation, evaluator|
+      if evaluator.in_room.present?
+        reservation.room = build(:room, evaluator.in_room)
+      end
+    end
   end
 end
--- spec/factories/rooms.rb
+++ spec/factories/rooms.rb
@@ -3,5 +3,20 @@ FactoryBot.define do
     sequence(:code, '01')
     capacity { rand(1..10) }
     notes { Faker::Lorem.sentence(word_count: 6) }
+
+    transient do
+      with_reservations { [] }
+    end
+
+    after(:build) do |room, evaluator|
+      if evaluator.with_reservations.present?
+        reservations = evaluator.with_reservations
+
+        reservations.each do |reservation|
+          reservation[:room] = room
+          create(:reservation, reservation)
+        end
+      end
+    end
   end
 end
