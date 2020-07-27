be42f37698f0fafdec680c13a1495954d058ed1b
Lucas Portela <lucasfjportela@gmail.com>
2020-07-27 01:15:49 -0300

    test: Improve integration tests of Reservation model

--- spec/system/reservations_spec.rb
+++ spec/system/reservations_spec.rb
@@ -7,17 +7,70 @@ RSpec.describe 'Reservations', type: :system do
 
   describe 'searching' do
     before do
-      Room.create!(code: '101', capacity: 2)
+      @room_101 = create_room(code: '101', capacity: 3)
+      @room_102 = create_room(
+        code: '102',
+        capacity: 2,
+        with_reservations: [{ start_date: '2020-07-30', end_date: '2020-08-05'}]
+      )
     end
 
-    it 'allows users to search for available rooms with a given capacity in a period' do
+    context 'when there are none available rooms' do
+      it 'show the standard system message' do
+        visit search_reservations_path
+
+        expect(page).to have_content('New Reservation')
+
+        fill_in 'From', with: '07/20/2020'
+        fill_in 'To', with: '08/20/2020'
+        select '5', from: '# of guests'
+        click_on 'Search for Available Rooms'
+
+        expect(page).to have_content('Available Rooms')
+
+        within('table') do
+          within('thead') do
+            expect(page).to have_content('Code')
+            expect(page).to have_content('Capacity')
+            expect(page).to have_content('Actions')
+          end
+
+          within('tbody') do
+            expect(page).to have_no_content(@room_101.code)
+            expect(page).to have_no_content(@room_102.code)
+            expect(page).to have_content('There are no available rooms for the selected filters')
+          end
+        end
+      end
+    end
+
+    context 'when user tries to search with start date after end date' do
+      it 'shows no results and show an alert' do
+        visit search_reservations_path
+
+        expect(page).to have_content('New Reservation')
+
+        fill_in 'From', with: '07/20/2020'
+        fill_in 'To', with: '07/18/2020'
+        select '5', from: '# of guests'
+        click_on 'Search for Available Rooms'
+
+        expect(page).to have_no_content('Available Rooms')
+        expect(page).to have_no_content(@room_101.code)
+        expect(page).to have_no_content(@room_102.code)
+
+        expect(page).to have_content("Initial date should be before the end date.")
+      end
+    end
+
+    it 'allow users to search for available rooms with a given capacity in a period' do
       visit search_reservations_path
 
       expect(page).to have_content('New Reservation')
 
-      fill_in 'From', with: '08/20/2020'
-      fill_in 'To', with: '08/23/2020'
-      select '2', from: '# of guests'
+      fill_in 'From', with: '07/20/2020'
+      fill_in 'To', with: '08/20/2020'
+      select '3', from: '# of guests'
       click_on 'Search for Available Rooms'
 
       expect(page).to have_content('Available Rooms')
@@ -30,10 +83,96 @@ RSpec.describe 'Reservations', type: :system do
         end
 
         within('tbody tr:first-child') do
-          expect(page).to have_content('101')
-          expect(page).to have_content('2 people')
+          expect(page).to have_no_content(@room_102.code)
+          expect(page).to have_content(@room_101.code)
+          expect(page).to have_content("#{@room_101.capacity} people")
           expect(page).to have_link('Create Reservation')
         end
+      end
+    end
+
+    it 'allow users to go back to the rooms list' do
+      visit search_reservations_path
+
+      expect(page).to have_link('Back', href: root_path)
+    end
+  end
+
+  describe 'new reservation' do
+    before do
+      @room_101 = create_room(
+        code: '101',
+        capacity: 5,
+        with_reservations: [{ start_date: '2020-07-30', end_date: '2020-08-05'}]
+      )
+    end
+
+    it 'allows users to create a new reservation' do
+      visit search_reservations_path
+
+      fill_in 'From', with: '07/20/2020'
+      fill_in 'To', with: '07/30/2020'
+      select '3', from: '# of guests'
+      click_on 'Search for Available Rooms'
+
+      search_request = "#{URI.parse(current_url).path}?#{URI.parse(current_url).query}"
+
+      within('table') do
+        within('thead') do
+          expect(page).to have_content('Code')
+          expect(page).to have_content('Capacity')
+          expect(page).to have_content('Actions')
+        end
+
+        within('tbody tr:first-child') do
+          click_link 'Create Reservation'
+        end
+      end
+
+      expect(page).to have_content("New Reservation")
+      expect(page).to have_selector('input[value="2020-07-20"]')
+      expect(page).to have_selector('input[value="2020-07-30"]')
+      expect(page).to have_selector('select option[selected][value="3"]')
+
+      fill_in 'Guest name', with: 'Fulano Teste'
+      click_on 'Create Reservation'
+
+      expect(page).to have_content("Room #{@room_101.code}")
+      expect(page).to have_content('was successfully created.')
+      expect(page).to have_content('2020-07-20 to 2020-07-30')
+    end
+
+    context 'when user tries to create a reservation with invalid values' do
+      it 'shows an alert with specific validation errors' do
+        visit search_reservations_path
+
+        fill_in 'From', with: '07/20/2020'
+        fill_in 'To', with: '07/30/2020'
+        select '3', from: '# of guests'
+        click_on 'Search for Available Rooms'
+
+        search_request = "#{URI.parse(current_url).path}?#{URI.parse(current_url).query}"
+
+        within('table') do
+          within('thead') do
+            expect(page).to have_content('Code')
+            expect(page).to have_content('Capacity')
+            expect(page).to have_content('Actions')
+          end
+
+          within('tbody tr:first-child') do
+            click_on 'Create Reservation'
+          end
+        end
+        
+        fill_in 'Start date', with: '07/31/2020'
+        fill_in 'End date', with: '07/30/2020'
+        select '7', from: 'Number of guests'
+        click_on 'Create Reservation'
+
+        expect(page).to have_content("Guest name can't be blank")
+        expect(page).to have_content('The start date should be before the end date')
+        expect(page).to have_content("Number of guests shouldn't be greater than room capacity")
       end
     end
   end
